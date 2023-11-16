5ed970741cf4f73691642389816aa3eb0a57dc10
Jack Dixon <jbd29@cam.ac.uk>
2023-11-16 21:20:20 +0000

    implemented catboost

--- catboost_classification.py
+++ catboost_classification.py
@@ -20,10 +20,13 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1, test_s
 
 train_dataset = cb.Pool(data=X_train, label=y_train)
 test_dataset = cb.Pool(data=X_test, label=y_test)
+cv_dataset = cb.cv(cb.Pool(data=X, label=y), params={'loss_function': cb.metrics.Logloss()})
 # Construct model:
 #iterations=1000, depth=6, learning_rate=0.1,
-model = cb.CatBoostClassifier(loss_function='Logloss', verbose=True)
+model = cb.CatBoostClassifier(loss_function='Logloss', verbose=True, learning_rate=0.1)
 model.fit(train_dataset)
 predictions = model.predict(test_dataset)
 pred_prob = model.predict_proba(test_dataset)
-print("Accuracy = {}%".format(np.round(accuracy_score(y_test, predictions),3) * 100))
+print(cv_dataset)
+print('Cross validation accuracy = {}%'.format(100*np.round(np.max(cv_dataset['test-Accuracy-mean']))))
+print("1-fold accuracy = {}%".format(np.round(accuracy_score(y_test, predictions),3) * 100))
